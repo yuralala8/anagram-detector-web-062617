b4ecd545eed0a5552fae9132daa2b8412e49553a
yuralala8 <yc1435@nyu.edu>
2017-07-03 21:46:25 -0400

    anagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    anagrams = []
+    array.each do |word|
+    if word.split('').sort == self.word.split('').sort
+      anagrams << word
+      end
+    end
+    anagrams
+  end
+
+end
